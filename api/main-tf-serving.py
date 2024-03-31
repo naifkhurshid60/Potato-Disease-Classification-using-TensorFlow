eed957c56307c8317d0e454e46bfa612e151653a
Naif Khurshid <naifkhurshid60@gmail.com>
2024-04-01 00:46:12 +0530

    files added

---
+++ api/main-tf-serving.py
@@ -0,0 +1,38 @@
+
+from fastapi import FastAPI,UploadFile,File
+import uvicorn
+import numpy as np
+from io import BytesIO
+from PIL import Image
+import tensorflow as tf
+
+
+app=FastAPI()
+endpoint = "http://localhost:8501/v1/models/potatoes_model:predict"
+#MODEL=tf.keras.models.load_model("../saved_models/2")
+CLASS_NAMES=["Early Blight","Late Blight","Healthy"]
+@app.get("/ping")
+async def ping():
+    return "hello"
+def read_file_as_image(data)->np.ndarray:
+    image=np.array(Image.open(BytesIO(data)))
+
+    return image
+@app.post("/predict")
+async def predict(
+        file:UploadFile=File(...)
+
+):
+   image=read_file_as_image( await file.read())
+   image_batch=np.expand_dims(image,0)
+   predictions=MODEL.predict(image_batch)
+   index=np.argmax(predictions)
+   predicted_class=CLASS_NAMES[index]
+   confidence=np.max(predictions[0])
+   print(predicted_class,"-> ",confidence)
+   return {"class":predicted_class,
+           "confidence":float(confidence)}
+   pass
+if __name__=="__main__":
+    uvicorn.run(app,host='localhost',port=8000)
+
